ce39bc10dc7ddae5ecb8e3b838f9f6e548145f25
kisya1488 <zekasakura111@yandex.ru>
2016-05-17 21:25:17 +0300

    Create files of classes cospreob and windows, fixed 70 error with vector type

--- include/k52/dsp/Imfcc.h
+++ include/k52/dsp/Imfcc.h
@@ -5,7 +5,9 @@
 #include <cmath>
 #include <vector>
 #include <complex>
-#include <c:/k52/k52/include/k52/dsp/transform/i_fourier_transform.h>
+#include <k52/dsp/transform/i_fourier_transform.h>
+#include <k52/dsp/windows.h>
+#include <k52/dsp/cosin_preobrasovanie.h>
 #include <memory>
 
 
@@ -13,44 +15,25 @@ namespace k52
 {
 	namespace dsp
 	{
-		class IMelFrequiencyCepstralCoefficients :public TringleWindow,public CosPreobr
+		class IMelFrequiencyCepstralCoefficients :public Window,public CosPreobr
 		{
 		public:
-			IMelFrequiencyCepstralCoefficients(const std::vector< double >&, IFourierTransform& ,double);
+			IMelFrequiencyCepstralCoefficients(const std::vector< double >, boost::shared_ptr <IFourierTransform>,double);
 			~IMelFrequiencyCepstralCoefficients();
 			std::vector<double> GetMFCC();		 //получить все коэффициенты
-			std::vector<double> GetMFCC(int);	 //получить определенное количество коэффициентов
-			void			    Add_zvuk(const std::vector< double >& , bool); //вызывается в тех случаях когда нужно перепостроить коэффициенты по новым данным
-			std::vector<double> Add_zvuk(const std::vector< double >&,int);
+			std::vector<double> GetMFCC(unsigned int);	 //получить определенное количество коэффициентов
+			void			    Add_zvuk(const std::vector< double >, size_t, bool); //вызывается в тех случаях когда нужно перепостроить коэффициенты по новым данным
+			std::vector<double> Add_zvuk(const std::vector< double >, size_t);
 		private:
-			std::vector< double >EnergyTringle(std::vector< double >);
+			std::vector< double >EnergyTringle(std::vector< double >&, size_t);
 			template <typename T > T FrecInMel(T);
 			template <typename T > T MelInFrec(T);
 			double epselon;							//ошибка, используется в analizing
-			shared_ptr <IFourierTransform> FFM;		//интерфейс фурье
+			boost::shared_ptr <IFourierTransform> FFM;		//интерфейс фурье
 			const std::vector< double >& sequence;	//вектор звука, удаляется после обработки в Add_zvuk
 			std::vector< double > MFCC;				//сами коэфициенты, удаляется в деструкторе
-			
-			//может не нужно (если делать с сохранением то тратятся ресурсы памяти если каждый раз считать то скорость работы)? удал в деструкторе
 			void analising();
 			std::vector<double> GetModulOfComplex(std::vector< std::complex<double>>);
-		};
-
-		class TringleWindow
-		{
-		public:
-			template <typename T> double TringleWindow::tringleWindow(T, T, T);
-		};
-
-
-
-		class CosPreobr
-		{
-		public:
-			std::vector<double> getCosPreobr1(std::vector<double>);
-			std::vector<double> getCosPreobr2(std::vector<double>);
-			std::vector<double> getCosPreobr3(std::vector<double>);
-			std::vector<double> getCosPreobr4(std::vector<double>);
 		};
 	} // namespace dsp
 } // namespace k52
--- src/dsp/Imfcc.cpp
+++ src/dsp/Imfcc.cpp
@@ -1,46 +1,47 @@
-#include "Imfcc.h"
-//#include "Math.h"
-
+#include "k52/dsp/Imfcc.h"
 
 namespace k52
 {
 	namespace dsp
 	{
 		IMelFrequiencyCepstralCoefficients::IMelFrequiencyCepstralCoefficients
-			(const std::vector<double>& seque, IFourierTransform & Furie,double epsel):sequence(seque),FFM(Furie)
+			(const std::vector<double> seque,boost::shared_ptr <IFourierTransform> Furie,double epsel):sequence(seque)//,FFM(Furie)
 		{
-			//уже сохранили интерфейс фурье преобразования.
+
+			FFM = Furie;//сохранили интерфейс фурье преобразования.
 			epselon = epsel;//допустимая ошибка
 			Add_zvuk(sequence,4);//получаем коэфициенты
 		};
 		IMelFrequiencyCepstralCoefficients::~IMelFrequiencyCepstralCoefficients () 
 		{		
-			MFCC.erase;
-			//FFM.
+			//MFCC.erase;
+			FFM->~IFourierTransform();
 		};
 		
 		std::vector <double> IMelFrequiencyCepstralCoefficients::GetMFCC()//сделан				
 		{
-			return GetMFCC(MFCC.size);
+			return GetMFCC(MFCC.size());
 		};	
 		//сделан
-		std::vector <double> IMelFrequiencyCepstralCoefficients::GetMFCC(int i)		
+		std::vector <double> IMelFrequiencyCepstralCoefficients::GetMFCC(size_t i)
 		{
-			if (i > MFCC.size)
-				i = MFCC.size;
+			if (i > MFCC.size())
+				i = MFCC.size();
 			std::vector <double> a(i);
-			for (int j = 0;j < MFCC.size;j++)
+			for (size_t j = 0;j < MFCC.size();j++)
 				a[j] = MFCC[i];
 			return a;
 		}; 
 		//сделан
 		void IMelFrequiencyCepstralCoefficients::	analising()//аналитика высчитывающая что коэффициенты с определенного места являются лишними (допустим нули)
 		{
-			for (int i = 0;i < MFCC.size;i++)
+			size_t N = MFCC.size();
+			for (size_t i = 0;i < N;i++)
 			if (MFCC[i]<epselon)
 			{
 				bool verno = false;
-				for (int j = i + 1;j < MFCC.size;j++)
+
+				for (size_t j = i + 1;j < N ;j++)
 					if (MFCC[j]>epselon)
 					{
 						verno = true;
@@ -56,9 +57,10 @@ namespace k52
 		//сделан
 		std::vector <double> IMelFrequiencyCepstralCoefficients::	GetModulOfComplex(std::vector< std::complex<double>> vhod) 
 		{
-			std::vector <double> vuhod(vhod.size);
-			for (int i = 0;i < vhod.size;i++)
-				vuhod[i] = vhod[i].imag*vhod[i].imag + vhod[i].real*vhod[i].real;
+			size_t N = vhod.size();
+			std::vector <double> vuhod(N);
+			for (size_t i = 0;i < N;i++)
+				vuhod[i] = abs(vhod[i]);
 			return vuhod;
 		};
 		//сделан
@@ -72,109 +74,54 @@ namespace k52
 			return (T)(700 * (exp(m / 1127.01048) - 1));
 		};
 		//сделан
-		void IMelFrequiencyCepstralCoefficients::Add_zvuk(const std::vector< double >& vhod, int rasOk,/*размер окна*/,bool prost)//сердце метода
+		void IMelFrequiencyCepstralCoefficients::Add_zvuk(const std::vector< double > vhod, size_t rasOk,/*размер окна*/bool prost)//сердце метода
 		{
-			std::vector< std::complex<double>> WithFurie=FFM.Transform(vhod);//WTF? why incoming in complex?!
+			size_t N = vhod.size();
+			std::vector< std::complex<double>> M(N);
+			for (size_t i = 0;i < N ;i++)
+				M[i] = vhod[i];
+			std::vector< std::complex<double>> WithFurie = FFM->Transform(M);//vhod);//WTF? why incoming in complex?!
 			std::vector <double> FurieDouble=GetModulOfComplex(WithFurie);//берем модуль элементов полученного массива
 		
 
 			//охапка MFCC и плов готов
-			MFCC = EnergyTringleMFCC(FurieDouble,KolOkon); //умножаем на окно, где его же и находим и сохраняем
+			MFCC = EnergyTringle(FurieDouble,rasOk); //умножаем на окно, где его же и находим и сохраняем
 			MFCC = getCosPreobr2(MFCC);//берем второе косинусное преобразование
 			
 			//конец и очистка мусора
-			WithFurie.erase;
-			sequence.clear;
+//			WithFurie.erase;
+			//sequence.clear;
 		};
-		std::vector<double> IMelFrequiencyCepstralCoefficients::Add_zvuk(const std::vector< double >&vhod,int RasmOkna) 
+		std::vector<double> IMelFrequiencyCepstralCoefficients::Add_zvuk(const std::vector< double >vhod, size_t RasmOkna)
 		{
 			Add_zvuk(vhod, RasmOkna,true);
 			analising();
 			return MFCC;
 		};
-		//сделан
-		//--------------------------------------------------------------------------------------------------
-		std::vector<double> CosPreobr::getCosPreobr1(std::vector<double> vhod)
+		std::vector< double >IMelFrequiencyCepstralCoefficients::EnergyTringle(std::vector< double >& vhod, size_t rasOk)
 		{
-			std::vector <double> vuhod(vhod.size);
-			for (int i = 0;i < vhod.size;i++)
-			{
-				if (i%2==0)
-					vuhod[i] = 0.5*(vhod[0] + vhod[vhod.size - 1]);
-				else
-				vuhod[i] = 0.5*(vhod[0]+(-1)*vhod[vhod.size-1]);
-				for (int j = 1;j < vhod.size - 1;j++)
-					vuhod[i] += vhod[j]*cos(14159265358979323846 * j*i / (vhod.size - 1));
-			}
-			return vuhod;
-		};
-		std::vector<double> CosPreobr::getCosPreobr2(std::vector<double> vhod)
-		{
-			std::vector <double> vuhod(vhod.size);
-			for (int i = 0;i < vhod.size;i++)
-				for (int j = 0;j < vhod.size;j++)
-					vuhod[i] += vhod[j] * cos(3.14159265358979323846*i*(j+0.5)/vhod.size);
-				return vuhod;
-		};
-		std::vector<double> CosPreobr::getCosPreobr3(std::vector<double> vhod)
-		{
-			std::vector <double> vuhod(vhod.size);
-			for (int i = 0;i < vhod.size;i++)
-			{
-				vuhod[i] = 0.5*vhod[0];
-				for (int j = 1;j < vhod.size-1;j++)
-					vuhod[i] += vhod[j] * cos(3.14159265358979323846*j*(i+0.5) / vhod.size);
-			}
-			return vuhod;
-		};
-		std::vector<double> CosPreobr::getCosPreobr4(std::vector<double> vhod)
-		{
-			std::vector <double> vuhod(vhod.size);
-			for (int i = 0;i < vhod.size;i++)
-				for (int j = 0;j < vhod.size;j++)
-					vuhod[i] += vhod[j] * cos(3.14159265358979323846*(i+0.5)*(j + 0.5) / vhod.size);
-			return vuhod;
-		};
-		//сделано
-		//--------------------------------------------------------------------------------------------------
-		template <typename T> double TringleWindow::tringleWindow(T k1,T k3,T tt)
-		{
-			T result = 0;
-			T k2 = (k3 - k1) / 2 + k1;
-			if ((tt <= k1) || (tt >= k3))
-				result = 0;
-			else if (tt < k2)
-				result = (tt - k1) / (k2 - k1);
-			else result = (k3-tt) / (k3-k2);
-			return result;
-		};
-		//--------------------------------------------------------------------------------------------------
-		std::vector< double >IMelFrequiencyCepstralCoefficients::EnergyTringle(std::vector< double > vhod, int rasOk)
-		{
-			int KolOkon = FrecInMel(vhod.size) / rasOk-1;//находим количество окон
+			size_t N = vhod.size();
+			size_t KolOkon = FrecInMel(N) / rasOk-1;//находим количество окон
 			std::vector <double> Energia(KolOkon);
-			std::vector <double> mel(vhod.size);
-			for (int i = 1;i <= vhod.size;i++)
-				mel = FrecInMel(i);
+			std::vector <double> mel(N);
+			for ( double i = 1;i <= N;i++)
+				mel[i-1] = FrecInMel(i);
 			//double nachaloM = mel[0];
-			int nachaloF = 0,sredina;
-			for (int i = 0;i<KolOkon;i++)
+			size_t nachaloF = 0,sredina;
+			for (size_t i = 0;i<KolOkon;i++)
 			{
 				double sum = 0;
-				int j;
+				size_t j;
 				for (j = mel[nachaloF]+1; mel[j] < mel[nachaloF] + rasOk * 2 ; j++);
 				for ( sredina = nachaloF + 1;mel[sredina]<mel[nachaloF]+(mel[nachaloF]-mel[j])/2;sredina++);
-				for (int u=nachaloF;u<=j;u++)
+				for (size_t u=nachaloF;u<=j;u++)
 					sum += vhod[u] * vhod[u] * tringleWindow(mel[nachaloF] , mel[j] , mel[u]);
 				nachaloF = j;
 				Energia[i] = log(sum);
 			}
 
-
+			return Energia;
 		};
 		
-		//--------------------------------------------------------------------------------------------------
-
-
 	} // namespace dsp
 } // namespace k52
